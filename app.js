6d61244f694a4f207efc6cd25527fafb7ecedcca
Haliun <haliungnbt31@gmail.com>
2020-11-27 13:06:14 +0800

    Shoo hayh

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ var activePlayer = 1;
 var scores = [0, 0];
 // Тоглогчын ээлжийн оноог хадгалах хувьсагч
 var roundScore = 0;
-// Шоо буух болгонд аль талаараа буухыг хадгалах, 1-6 гэсэн утгыг хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6)+ 1;
-console.log("Шоо:" + dice);
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').innerHTML = 0;
-document.querySelector('#current-0').innerHTML = 0;
-document.querySelector('#current-1').innerHTML = 0;
 
-document.querySelector('.dice').style.display = "none";
+// Программ эхлэхэд бэлдэх
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+document.querySelector(".dice").style.display = "none";
+
+// Шоо буух болгонд аль талаараа буухыг хадгалах, 1-6 гэсэн утгыг хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    var diceNumber = Math.floor(Math.random() * 6)+ 1;
+    document.querySelector(".dice").style.display = "block";
+    document.querySelector(".dice").src = "dice-" + diceNumber + ".png";
+});
